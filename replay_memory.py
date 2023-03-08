03c0ea30693c2de88be8262413d911d1c29fa533
NM00200 <nickwd516@gmail.com>
2023-03-08 18:03:33 +0000

    adding the continuous KL case

--- replay_memory.py
+++ replay_memory.py
@@ -71,7 +71,7 @@ class DiscreteMemory:
             self.position = len(self.buffer) % self.capacity
 
 
-class ContinuousReplayMemory:
+class ContinuousMemory:
     def __init__(self, capacity, gamma, seed):
         random.seed(seed)
         self.capacity = capacity
@@ -85,35 +85,39 @@ class ContinuousReplayMemory:
             self.buffer.append(None)
             self.priority_mem.append(None)
 
-        self.position = int(self.position)
-
         self.buffer[self.position] = (state, preference, action, reward, next_state, next_preference, done)
 
-        ############################################################################
-        Q_val = agent.critic_target(torch.FloatTensor(state.reshape(1,-1)), preference.reshape(1,-1), torch.FloatTensor(np.array([action])))[0]
-        hq_0 = agent.critic_target(torch.FloatTensor(next_state.reshape(1,-1)), next_preference.reshape(1,-1), torch.FloatTensor(np.array([[-1.0,-1.0,-1.0]])))[0]
-        hq_1 = agent.critic_target(torch.FloatTensor(next_state.reshape(1,-1)), next_preference.reshape(1,-1), torch.FloatTensor(np.array([[1.,1.,1.]])))[0]
-        ############################################################################
+        Q_val_0, Q_val_1 = agent.critic_target(state.unsqueeze(0), preference.unsqueeze(0), action.unsqueeze(0))
+        
+        Q_val = torch.min(Q_val_0, Q_val_1)
 
-        hq = torch.max(hq_0, hq_1)
-        wr = preference.dot(torch.FloatTensor(reward))
+        hq_0, hq_1 = agent.critic_target(state.unsqueeze(0), preference.unsqueeze(0), action.unsqueeze(0))
+        hq = torch.min(hq_0, hq_1)
+        hq = torch.max(hq)
+
+        wr = preference.dot(reward)
         p = abs(wr + done*self.gamma * hq - Q_val)
-        self.priority_mem[self.position] = p.detach().cpu().numpy()[0]
+
+        self.priority_mem[self.position] = p.detach().cpu().numpy()
 
         self.position = (self.position + 1) % self.capacity
 
     def sample(self, batch_size):
         pri =  np.array(self.priority_mem)
-        pri = pri/pri.sum()
+        pri = pri / pri.sum()
+
+        pri = pri.flatten()
 
         index_list = np.random.choice(
             range(len(self.buffer)), batch_size,
             replace=False,
             p=pri
         )
-        # batch = random.sample(self.buffer, batch_size)
+
         batch = [self.buffer[i] for i in index_list]
+
         state, preference, action, reward, next_state, next_preference, done = map(np.stack, zip(*batch))
+
         return state, preference, action, reward, next_state, next_preference, done
 
     def __len__(self):
@@ -136,3 +140,4 @@ class ContinuousReplayMemory:
         with open(save_path, "rb") as f:
             self.buffer = pickle.load(f)
             self.position = len(self.buffer) % self.capacity
+
