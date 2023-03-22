7ca1d3526ab0711750e87b756bacad47c50a0b5c
NM00200 <nickwd516@gmail.com>
2023-03-22 10:55:10 +0000

    changing argument defaults

--- main_continuous.py
+++ main_continuous.py
@@ -20,17 +20,17 @@ parser.add_argument('--env_name', default="mo-hopper-v4",
                     help='MOGYM enviroment (default: mo-hopper-v4)')
 parser.add_argument('--gamma', type=float, default=0.99, metavar='G',
                     help='discount factor for reward (default: 0.99)')
-parser.add_argument('--tau', type=float, default=0.01, metavar='G',
-                    help='target smoothing coefficient(τ) (default: 0.01)')
+parser.add_argument('--tau', type=float, default=0.005, metavar='G',
+                    help='target smoothing coefficient(τ) (default: 0.005)')
 parser.add_argument('--lr', type=float, default=1e-4, metavar='G',
                     help='learning rate (default: 1e-4)')
 parser.add_argument('--seed', type=int, default=123, metavar='N',
                     help='random seed (default: 123)')
-parser.add_argument('--batch_size', type=int, default=128, metavar='N',
-                    help='batch size (default: 128)')
+parser.add_argument('--batch_size', type=int, default=256, metavar='N',
+                    help='batch size (default: 256)')
 parser.add_argument('--num_steps', type=int, default=int(1.5e7), metavar='N',
                     help='maximum number of steps (default: 1.5e6)')
-parser.add_argument('--num_episodes', type=int, default=10000, metavar='N',
+parser.add_argument('--num_episodes', type=int, default=100000, metavar='N',
                     help='maximum number of episodes (default: 3000)')
 parser.add_argument('--hidden_size', type=int, default=512, metavar='N',
                     help='hidden size (default: 512)')
@@ -40,8 +40,8 @@ parser.add_argument('--start_steps', type=int, default=10000, metavar='N',
                     help='Steps sampling random actions (default: 10000)')
 parser.add_argument('--target_update_interval', type=int, default= 1, metavar='N',
                     help='Value target update per no. of updates per step (default: 1)')
-parser.add_argument('--replay_size', type=int, default=50000, metavar='N',
-                    help='size of replay buffer (default: 10000)')
+parser.add_argument('--replay_size', type=int, default=2e6, metavar='N',
+                    help='size of replay buffer (default: 2e6)')
 parser.add_argument('--cuda', action="store_true",
                     help='run on CUDA (default: False)')
 parser.add_argument('--mps', action="store_true",
